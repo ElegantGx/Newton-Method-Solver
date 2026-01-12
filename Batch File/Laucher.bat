15587d712b0d8f3e46a39b243eb85dcadbe1b71a
G(x) <2050230094@qq.com>
2026-01-12 10:05:40 +0800

    Update .bat

---
+++ Batch File/Laucher.bat
@@ -0,0 +1,27 @@
+@echo off 
+if not "%1"=="min" start /min cmd /c "%~0" min & exit
+cd  /d "%~dp0"
+
+msg * "正在检查java环境"
+java -version >nul 2>&1
+if %errorlevel% neq 0 (
+    msg * "检测到未安装Java环境"
+    msg * "请根据指引安装JDK至默认目录"
+    msg * "tips:请中途选择自动配置"
+    start /wait jdk_installer.msi
+    pause
+    exit /b 1
+)
+msg * "java环境正常"
+if exist "com\gx\*.class" (
+   del /q com\gx\*.class
+)
+if exist "*.txt" (
+   del /q *.txt 
+)
+copy "File\*.txt""" 
+msg * "正在编译源码"
+msg * "Loading......"
+timeout /t 5 /nobreak >nul
+start run.bat
+exit
